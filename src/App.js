edaecf97bfb811a54ffecee4780607183287a13d
jsoncummings <cummings.jason.r@gmail.com>
2022-08-05 17:56:49 -0400

    hello world

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import { ItemTable } from './ItemTable/ItemTable';
 
 function App() {
+  const filesArray = [
+    { name: 'smss.exe', device: 'Stark', path: '\\Device\\HarddiskVolume2\\Windows\\System32\\smss.exe', status: 'scheduled' },
+    { name: 'netsh.exe', device: 'Targaryen', path: '\\Device\\HarddiskVolume2\\Windows\\System32\\netsh.exe', status: 'available' },
+    { name: 'uxtheme.dll', device: 'Lannister', path: '\\Device\\HarddiskVolume1\\Windows\\System32\\uxtheme.dll', status: 'available' },
+    { name: 'cryptbase.dll', device: 'Martell', path: '\\Device\\HarddiskVolume1\\Windows\\System32\\cryptbase.dll', status: 'scheduled' },
+    { name: '7za.exe', device: 'Baratheon', path: '\\Device\\HarddiskVolume1\\temp\\7za.exe', status: 'scheduled' }
+  ];
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ItemTable itemsArray={filesArray}></ItemTable>
     </div>
   );
 }
---
+++ src/ItemTable/ItemTable.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const ItemTable = (props) => {
+    console.log('hi',props.itemsArray);
+    return (
+        <div>
+            <span>Hello World</span>
+        </div>
+    )
+}
