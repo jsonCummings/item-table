4cc54a840adcd1ec2ba16adba0ce09b867209725
jsoncummings <cummings.jason.r@gmail.com>
2022-08-05 19:14:08 -0400

    added checkbox w 3 states

--- src/ItemTable/ItemTable.jsx
+++ src/ItemTable/ItemTable.jsx
@@ -1,22 +1,29 @@
-import React, {useState} from 'react';
+import React, {useCallback, useState} from 'react';
 import './itemTable.css';
 
 export const ItemTable = (props) => {
   const [itemsArray, setItems] = useState(props.itemsArray);
   const [selectedItems, setSelectedItems] = useState([]);
 
-
   const allSelected = () => {
     return itemsArray.every((item => item?.checked && item.checked))
   }
+  const someSelected = () => {
+    return !!(selectedItems.length && (itemsArray.length !== selectedItems.length));
+  }
+  const indeterminateState = useCallback( el => {
+    console.log('flip table', el, someSelected());
+    if (el && someSelected()) {
+        el.indeterminate = someSelected();
+    } 
+    return false;
+  }, [selectedItems]);
 
   const checkIfAvailable = (itemStatus) => {
     return itemStatus.toLowerCase().indexOf('available') > -1;
   }
 
   const selectAllItems = () => {
-    // if all are selected unselect
-    // if not all selected will turn all items to selected state
     if(allSelected()) {
       setItems(items => (
         items.map( item => {
@@ -35,8 +42,7 @@ export const ItemTable = (props) => {
   }
 
   const handleCheckEvent = (item) => {
-    // console.log('clicked', item, checked);
-    const {name, device, path, status, checked} = item;
+    const {name, checked} = item;
     setItems(items => (
       items.map( item => {
         if (item.name === name) {
@@ -56,52 +62,57 @@ export const ItemTable = (props) => {
     // console.log('items after set', items, selectedItems);
   }
 
-    console.log('render', itemsArray, selectedItems);
-    return (
-        <div>
-            <table className="itemTable">
-                <thead>
-                    <tr>
-                        {/* needs all selected, some selected, none selected   value={selectAllInput}*/}
-                        <th>
-                          <input type="checkbox" onClick={() => selectAllItems()}/>
-                        </th>
-                        <th></th>
-                        <th>
-                          <span>&#10515;</span> Download Selected
-                        </th>
-                    </tr>
-                    <tr>
-                        <th/>
-                        <th>Name</th>
-                        <th>Device</th>
-                        <th>Path</th>
-                        <th/>
-                        <th>Status</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {itemsArray && itemsArray.map((item) =>{
-                        const {name, device, path, status, checked} = item;
-                        return (
-                            <tr key={name}>
-                                <td>
-                                    <input type="checkbox"
-                                        checked={checked}
-                                        onChange={e => handleCheckEvent(item)} 
-                                        value={name}
-                                    />
-                                </td>
-                                <td>{name}</td>
-                                <td>{device}</td>
-                                <td>{path}</td>
-                                <td>{checkIfAvailable(status) ? <span className="green-dot"></span> : null}</td>
-                                <td>{status}</td>
-                            </tr>
-                        )
-                    })}
-                </tbody>
-            </table>
-        </div>
+  console.log('render', itemsArray, selectedItems);
+  return (
+    <div>
+        <table className="itemTable">
+            <thead>
+                <tr>
+                    {/* needs all selected, some selected, none selected*/}
+                    <th>
+                        {someSelected() && (<input type="checkbox"
+                        ref={indeterminateState}
+                        onClick={() => selectAllItems()}/>)}
+                        {!someSelected() && (<input type="checkbox"
+                        checked={allSelected()}
+                        onClick={() => selectAllItems()}/>)}
+                    </th> 
+                    <th></th>
+                    <th>
+                      <span>&#10515;</span> Download Selected
+                    </th>
+                </tr>
+                <tr>
+                    <th/>
+                    <th>Name</th>
+                    <th>Device</th>
+                    <th>Path</th>
+                    <th/>
+                    <th>Status</th>
+                </tr>
+            </thead>
+            <tbody>
+                {itemsArray && itemsArray.map((item) =>{
+                    const {name, device, path, status, checked} = item;
+                    return (
+                        <tr key={name}>
+                            <td>
+                                <input type="checkbox"
+                                    checked={checked}
+                                    onChange={e => handleCheckEvent(item)} 
+                                    value={name}
+                                />
+                            </td>
+                            <td>{name}</td>
+                            <td>{device}</td>
+                            <td>{path}</td>
+                            <td>{checkIfAvailable(status) ? <span className="green-dot"></span> : null}</td>
+                            <td>{status}</td>
+                        </tr>
+                    )
+                })}
+            </tbody>
+        </table>
+    </div>
   )
 }
